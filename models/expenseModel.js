a088f7c5276ec171761d873b6f245d2d570adca0
caique almeida <caaguima@gmail.com>
2021-01-11 00:18:59 -0300

    Implemented Virtual property on schema

--- models/expenseModel.js
+++ models/expenseModel.js
@@ -1,46 +1,56 @@
 const mongoose = require('mongoose');
 
-const expenseSchema = new mongoose.Schema({
-  category: {
-    type: String,
-    required: [true, 'A expense must have a category'],
-    trim: true,
+const expenseSchema = new mongoose.Schema(
+  {
+    category: {
+      type: String,
+      required: [true, 'A expense must have a category'],
+      trim: true,
+    },
+    value: {
+      type: Number,
+      required: [true, 'A expense must have a value'],
+    },
+    description: {
+      type: String,
+      required: [true, 'A expense must have a description'],
+      trim: true,
+    },
+    date: {
+      type: Date,
+      required: [true, 'A expense must have a date'],
+    },
+    observation: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+    instalments: {
+      type: Number,
+      required: false,
+    },
+    type: {
+      type: String,
+      required: [true, 'A expense must have a type'],
+      trim: true,
+    },
+    tags: {
+      type: [String],
+      required: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
   },
-  value: {
-    type: Number,
-    required: [true, 'A expense must have a value'],
-  },
-  description: {
-    type: String,
-    required: [true, 'A expense must have a description'],
-    trim: true,
-  },
-  date: {
-    type: Date,
-    required: [true, 'A expense must have a date'],
-  },
-  observation: {
-    type: String,
-    required: false,
-    trim: true,
-  },
-  instalments: {
-    type: Number,
-    required: false,
-  },
-  type: {
-    type: String,
-    required: [true, 'A expense must have a type'],
-    trim: true,
-  },
-  tags: {
-    type: [String],
-    required: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+expenseSchema.virtual('instalmentValue').get(function () {
+  return this.value / this.instalments;
 });
 
 const Expense = mongoose.model('Expense', expenseSchema);
